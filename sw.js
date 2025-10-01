8a7f358e4e256e2b5feb42ae02af67bdcb909051
SSPL-71 <ssservicespl@gmail.com>
2025-10-01 20:05:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,5 +3,10 @@ self.addEventListener('install', () => {
 });
 
 self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // Bypass compression route to avoid interference
+  if (url.pathname === '/compress') return;
+
   // You can customize caching behavior here later
 });
